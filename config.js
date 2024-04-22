e38a290220e113e39bc54ecea8f3f9e1cc46f10f
Lumosion <2021904558@chd.edu.cn>
2024-04-23 00:55:11 +0800

    init

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Lumoison 站点监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2529992-857be6e4247108186bc6159e'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://lumosion.me'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/Lumosion'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.lumosion.me'
+    },
+  ],
+};
